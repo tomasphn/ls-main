6316d1efc6d65ebb5268152050083a1b0d959934
tomasphn@gmail.com <tpalpallatoc@DESKTOP-HRM5OK3.localdomain>
2022-08-04 15:57:51 -0700

    add car_loan.rb

---
+++ RB_101/lesson_2/car_loan.rb
@@ -0,0 +1,18 @@
+def valid_input(message)
+  puts message
+  loop do
+    ans = gets.chomp
+    ans.to_i.to_s == ans || ans == '0' ? (return ans.to_i) : (puts 'Please enter a valid number')
+  end
+end
+
+puts 'Welcome to mortgage calculator!'
+
+p loan_amnt = valid_input('Please enter your loan amount')
+p monthly_rate = (valid_input('Please enter your APR percentage as a whole number (ex: for 5%, enter 5)').to_f / 100) / 12
+p monthly_dur = valid_input('Please enter the duration of your loan in months')
+
+monthly_pay = loan_amnt * (monthly_rate / (1 - (1 + monthly_rate)**(-monthly_dur)))
+
+puts "Your payment is #{monthly_pay}$ a month"
+puts 'Thank you for using mortgage calculator'
