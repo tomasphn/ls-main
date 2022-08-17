8543b201577e9fd17022b945547a18f7ca00886f
tomasphn@gmail.com <tpalpallatoc@DESKTOP-HRM5OK3.localdomain>
2022-08-17 15:10:22 -0700

    finish tic_tac.rb first iteration

--- 101_RB/6_lesson/tic_tac.rb
+++ 101_RB/6_lesson/tic_tac.rb
@@ -1,12 +1,10 @@
+require "pry"
 INITIAL_MARKER = ' '
 PLAYER_MARKER = 'X'
 COMPUTER_MARKER = 'O'
 
-def prompt(msg)
-  puts "=> #{msg}"
-end
-
 def display_board(brd)
+  puts "You're a #{PLAYER_MARKER}, Computer is #{COMPUTER_MARKER}"
   puts "       |       |       "
   puts "   #{brd[1]}   |   #{brd[2]}   |   #{brd[3]}   "
   puts "       |       |       "
@@ -32,16 +30,66 @@ end
 def player_plays!(brd)
   square = ''
   loop do 
-    prompt "Choose a square (#{empty_squares(brd).join(', ')}):"
+    puts "Choose a square (#{empty_squares(brd).join(', ')}):"
     square = gets.chomp.to_i
     break if empty_squares(brd).include?(square)
-    prompt "Sorry, that's not a valid choice"
+    puts "Sorry, that's not a valid choice"
   end
 
   brd[square] = PLAYER_MARKER
 end
 
-board = initialize_board
-display_board(board)
-player_plays!(board)
-display_board(board)
+def computer_plays!(brd)
+  square = empty_squares(brd).sample
+  brd[square] = COMPUTER_MARKER
+end
+
+def board_full?(brd)
+  empty_squares(brd).empty?
+end
+
+def someone_won?(brd)
+  !!detect_winner(brd)
+end
+
+def detect_winner(brd)
+  winning_lines = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] +
+                  [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + 
+                  [[1, 5, 9], [3, 5, 7]]
+
+  winning_lines.each do |line|
+    if line.all? {|space| brd[space] == PLAYER_MARKER}
+      return 'Player'
+    elsif line.all? {|space| brd[space] == COMPUTER_MARKER}
+      return 'Computer'
+    end
+  end
+  nil
+end
+
+loop do
+  board = initialize_board
+  loop do
+    display_board(board)
+    player_plays!(board)
+    break if someone_won?(board) || board_full?(board)
+
+    computer_plays!(board)
+    break if someone_won?(board) || board_full?(board)
+  end
+
+  display_board(board)
+
+  if someone_won?(board)
+    puts "#{detect_winner(board)} won!"
+  else
+    puts "It's a tie!"
+  end
+
+  puts "Play again? (y or n)"
+  answer = gets.chomp
+  break if answer.upcase.start_with?('N')
+
+end
+
+puts "Thanks for playing!"
